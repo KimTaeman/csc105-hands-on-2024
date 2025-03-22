5697d8d8a21103ec0fb65fc7dfd282caafffed6c
Nang Hayman Aye Mya <nanghaymanayemya@gmail.com>
2025-03-22 13:45:20 +0700

    Fix again

--- profile-website/src/pages/Navbar.jsx
+++ profile-website/src/pages/Navbar.jsx
@@ -4,7 +4,7 @@ import Button from "../components/Button"
 const Navbar = () => {
   return (
     <>
-     <nav className="nav-wrapper w-full p-3 md:p-5 md:p-10 h-10 flex justify-between items-center text-xs md:text-sm">
+     <nav className="nav-wrapper w-full p-3  md:p-10 h-10 flex justify-between items-center text-xs md:text-sm">
           <a href="#" className="brand-logo text-sm md:text-2xl font-bold">Artist John</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down flex md:gap-6">
             <li className="md:py-1.5 px-3 md:px-5 text-center rounded-2xl  hover:bg-lime-100"><a href="#home">Home</a></li>
